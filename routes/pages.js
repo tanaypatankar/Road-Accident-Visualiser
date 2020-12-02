b8350dc8ac284e0081122d237bf398dddfc067cc
Satyajeet2000 <satyajeet.mspatil@gmail.com>
2020-12-02 11:11:56 +0530

    Form Commit Stable

--- routes/pages.js
+++ routes/pages.js
@@ -15,4 +15,20 @@ router.get('/visselection', (req,res) => {
     res.render('visselection.hbs', {email: user})
 } );
 
+
+
+//FOR INSERTION FORM
+router.get('/idx', (req, res) => {
+    res.render('idx'); 
+});
+router.get('/add', (req, res) => {
+    res.render('add');
+});
+router.get('/add_casualties', (req, res) => {
+    res.render('add_casualties'); 
+});
+router.get('/add_vehicles', (req, res) => {
+    res.render('add_vehicles');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const mysql = require("mysql");
+//const mysql = require("mysql");
 const dotenv = require("dotenv");
 const bodyparser = require('body-parser');
 let cors = require('cors')
@@ -29,8 +29,14 @@ app.use(cors())
 
 app.use('/', require('./routes/pages'));
 app.use('/vis', require('./routes/vis'));
-app.use('/users', require('./routes/users'))
+app.use('/users', require('./routes/users'));
 const router = express.Router();
+
+//AUTHORIZE FORM
+const authController = require('./controllers/auth'); 
+app.post("/auth/add", authController.register);
+app.post("/auth/add_casualties", authController.add_casualties);
+app.post("/auth/add_vehicles", authController.add_vehicles);
 
 // '/' directory of express
 router.get('/', (req,res) => {
@@ -41,6 +47,6 @@ module.exports = router;
 
 
 // Start express on port 5000
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
+app.listen(5009, () => {
+    console.log("Server started on port 5009");
 });
